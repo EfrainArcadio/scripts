ae8efaf50095fc81a4ab13dea7ed83931484b931
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-09-02 14:04:27 -0600

    2daqna-Ago

--- scripts/scriptsRecargas/quincenalRecargas.py
+++ scripts/scriptsRecargas/quincenalRecargas.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 ## Primer fecha compuesta de la consulta 
 y= "2024"
-mes = "Julio"
-m = "07"
+mes = "Agosto"
+m = "08"
 dia_in = 16
 dia_fn = 32
 # qna = '1ra'
@@ -13,6 +13,13 @@ ae = "-Transacciones-extension.csv"
 ###############################################################################
 #                           Carga de archivos                                 #
 ###############################################################################
+def path_verify(path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+    print(f'Directorio Creado: {path}')
+  else:
+    print(f'El Directorio ya existe: {path}')
+
 ## Obtener la ruta del directorio actual
 ruta_actual = os.path.dirname(__file__)
 ## Subir un nivel en el directorio
@@ -22,6 +29,8 @@ ruta_sup = os.path.dirname(ruta_sup_2)
 ##
 path_files = os.path.join(ruta_sup,f'dataFiles/recargas/{y}/{m} {mes}')
 ruta_dumps = os.path.join( ruta_sup_2,f'public/recargas/quincenas/{y}/{m} {mes}/{qna} qna')
+path_verify(ruta_dumps)
+
 ##
 archivo_tr = [os.path.join(path_files, f"{y}{m}{d:02d}{a}") for d in range(dia_in, dia_fn)]
 ## Leer Archivos de Extencion para obtener la duracion de las transacciones
@@ -35,7 +44,7 @@ for transaccion in archivo_tr:
   transacciones.append(df)
 df_transacciones = pd.concat(transacciones)
 print('Creando archivo Transacciones...')
-tr_file = f'Full_{qna}_qna_{mes}_{y}'
+tr_file = f'Full_{qna}_qna_{mes}_{y}.csv'
 ruta_res = os.path.join(ruta_dumps,tr_file)
 df_transacciones.to_csv(ruta_res,index=False)
 ## Concatenación de documentos extraídos del arreglo transacciones y creando un solo DataFrame con información de toda la quincena
@@ -47,7 +56,7 @@ for extencion in archivo_ex:
   extenciones.append(df)
 print('Creando archivo Extenciones...')
 df_extenciones = pd.concat(extenciones)
-ext_file = f'Full_ext_{qna}_qna_{mes}_{y}'
+ext_file = f'Full_ext_{qna}_qna_{mes}_{y}.csv'
 ruta_res = os.path.join(ruta_dumps,ext_file)
 df_extenciones.to_csv(ruta_res,index=False)
 ##
@@ -61,7 +70,7 @@ df_short_tr = df_transacciones[[
 ]]
 ##
 print('Creando archivo Short Transacciones...')
-short_file = f'{qna}_qna_{mes}_{y}{a}'
+short_file = f'{qna}_qna_{mes}_{y}{a}.csv'
 ruta_res = os.path.join(ruta_dumps,short_file)
 df_short_tr.to_csv(ruta_res,index=False)
 ##

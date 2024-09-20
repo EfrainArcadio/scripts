c370301c311065da34c20291c0c2450c98b38b4f
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-09-19 18:57:49 -0600

    Erase_trash_lines

--- loadFilesRecargas.py
+++ loadFilesRecargas.py
@@ -58,47 +58,45 @@ for extencion in archivo_ex:
   extenciones.append(df)   
 
 def uploadExt(df, table_name, connection):
-    try:
-        default_date = f'{y}-{m}-{dia_fn-1} 00:00:00'
-        df['START_DATE'] = df['START_DATE'].fillna(default_date)
-        df['END_DATE'] = df['END_DATE'].fillna(default_date)
-        columns = list(df.columns)
-        # print(columns)
-        unique_col = columns[0]
-        lowercase_columns = [name.lower() for name in columns]
-        cursor = connection.cursor()
-        datos = df.values
-        querry = f"""
-            INSERT INTO {table_name} ({", ".join(lowercase_columns)})
-            VALUES ({", ".join(["%s"] * len(lowercase_columns))})
-            ON CONFLICT ({unique_col}) DO NOTHING;
-        """
-        cursor.executemany(querry, datos.tolist())
-        connection.commit()
-        cursor.close()
-        print(f"Se han cargado {len(df)} filas a la tabla {table_name} del {mes}")
-    except Exception as e:
-        print(e.__class__.__name__, ":", e)
+  try:
+    default_date = f'{y}-{m}-{dia_fn-1} 00:00:00'
+    df['START_DATE'] = df['START_DATE'].fillna(default_date)
+    df['END_DATE'] = df['END_DATE'].fillna(default_date)
+    columns = list(df.columns)
+    unique_col = columns[0]
+    lowercase_columns = [name.lower() for name in columns]
+    cursor = connection.cursor()
+    datos = df.values
+    querry = f"""
+        INSERT INTO {table_name} ({", ".join(lowercase_columns)})
+        VALUES ({", ".join(["%s"] * len(lowercase_columns))})
+        ON CONFLICT ({unique_col}) DO NOTHING;
+    """
+    cursor.executemany(querry, datos.tolist())
+    connection.commit()
+    cursor.close()
+    print(f"Se han cargado {len(df)} filas a la tabla {table_name} del {mes}")
+  except Exception as e:
+    print(e.__class__.__name__, ":", e)
 ##
 def uploadTra(df, table_name, connection):
-    try:
-        
-        columns = list(df.columns)
-        unique_col = columns[0]
-        lowercase_columns = [name.lower() for name in columns]
-        cursor = connection.cursor()
-        datos = df.values
-        querry = f"""
-            INSERT INTO {table_name} ({", ".join(lowercase_columns)})
-            VALUES ({", ".join(["%s"] * len(lowercase_columns))})
-            ON CONFLICT ({unique_col}) DO NOTHING;
-        """
-        cursor.executemany(querry, datos.tolist())
-        connection.commit()
-        cursor.close()
-        print(f"Se han cargado {len(df)} filas a la tabla {table_name} del {mes}")
-    except Exception as e:
-        print(e.__class__.__name__, ":", e)
+  try:
+    columns = list(df.columns)
+    unique_col = columns[0]
+    lowercase_columns = [name.lower() for name in columns]
+    cursor = connection.cursor()
+    datos = df.values
+    querry = f"""
+        INSERT INTO {table_name} ({", ".join(lowercase_columns)})
+        VALUES ({", ".join(["%s"] * len(lowercase_columns))})
+        ON CONFLICT ({unique_col}) DO NOTHING;
+    """
+    cursor.executemany(querry, datos.tolist())
+    connection.commit()
+    cursor.close()
+    print(f"Se han cargado {len(df)} filas a la tabla {table_name} del {mes}")
+  except Exception as e:
+    print(e.__class__.__name__, ":", e)
 ##
 df_ext = pd.concat(extenciones)  
 df_tra = pd.concat(transacciones)
@@ -106,16 +104,15 @@ df_tra = pd.concat(transacciones)
 # Verificando si la columna existe y renombrando si es necesario
 # Eliminar la columna duplicada
 df_tra = df_tra.drop_duplicates()
-# print(df_tra.columns)
+
 if 'CONTRACT_SALE_SAME' in df_tra.columns:
-    df_tra.rename(columns={'CONTRACT_SALE_SAME': 'CONTRACT_SALE_SAM'}, inplace=True)
-# print(df_tra.columns)
+  df_tra.rename(columns={'CONTRACT_SALE_SAME': 'CONTRACT_SALE_SAM'}, inplace=True)
 
 df_tra = df_tra.loc[:, ~df_tra.columns.duplicated()]
-# print(columnas_duplicadas)
+
 df_tra = df_tra[['ID_TRANSACCION_ORGANISMO','PROVIDER','TIPO_TARJETA','NUMERO_SERIE_HEX','FECHA_HORA_TRANSACCION','TIPO_EQUIPO','LOCATION_ID','TIPO_TRANSACCION','SALDO_ANTES_TRANSACCION','MONTO_TRANSACCION','SALDO_DESPUES_TRANSACCION','SAM_SERIAL_HEX_ULTIMA_RECARGA','SAM_SERIAL_HEX','CONTADOR_RECARGAS','EVENT_LOG','LOAD_LOG','MAC','SAM_COUNTER','ENVIRONMENT','ENVIRONMET_ISSUER_ID','CONTRACT','CONTRACT_TARIFF','CONTRACT_SALE_SAM','CONTRACT_RESTRICT_TIME','CONTRACT_VALIDITY_START_DATE','CONTRACT_VALIDITY_DURATION']]
 df_tra = df_tra.drop_duplicates()
-# print(df_tra.columns)
+
 if connection:
   print("Conexión exitosa")
   print(f"Llenando tabla {tablaExtName} ...")

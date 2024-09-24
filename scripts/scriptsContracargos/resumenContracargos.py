7314080a233a1c8f66d306e19842734561207f4d
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-09-23 18:25:35 -0600

    rev_ctc_sem_38_&_updateScript

--- scripts/scriptsContracargos/resumenContracargos.py
+++ scripts/scriptsContracargos/resumenContracargos.py
@@ -2,70 +2,122 @@ import json
 import pandas as pd
 import os
 from fuzzywuzzy import fuzz
-##
-sem = '37'
+## localizacion de archivos
+sem = '38'
 y = '2024'
-file = 'sem_37_09-15_sep'
-file_bans = 'banned_mails.json'
-umbral_similitud=80
-##
-won = 'Ganados: '
-lost = 'Perdidos: '
-disputa = 'Disputa: '
+file = 'sem_38_16-22_sep'
+## archivos json correos baneados
+file_bans_v1 = 'banned_mails_v1.json'
+file_bans_v2 = 'banned_mails_v2.json'
+## archivos dumps
 file_dump = f'{sem}_AppCDMX_Analisis_Contracargos.xlsx'
 file_won = f'{sem}_won_contracargo.json'
 file_lost = f'{sem}_lost_contracargo.json'
 file_dispute = f'{sem}_dispute_contracargo.json'
-fecha_limit = '13-08-2024 23:59:59'
-fecha_limit = pd.to_datetime(fecha_limit, format='%d-%m-%Y %H:%M:%S')
-###############################################################################
-#                           Carga de archivos                                 #
-###############################################################################
-## Obtener la ruta del directorio actual
-ruta_actual = os.path.dirname(__file__)
-ruta_bans = os.path.join(ruta_actual,'data')
+## Fechas
+fecha_limit_1 = '13-08-2024 23:59:59'
+fecha_limit_1 = pd.to_datetime(fecha_limit_1, format='%d-%m-%Y %H:%M:%S')
+fecha_limit_2 = '20-09-2024 23:59:59'
+fecha_limit_2 = pd.to_datetime(fecha_limit_2, format='%d-%m-%Y %H:%M:%S')
+## Utilidades
+won = 'Ganados: '
+lost = 'Perdidos: '
+disputa = 'Disputa: '
 
-## Subir un nivel en el directorio
-ruta_superior = os.path.dirname(ruta_actual)
-ruta_sup_2 = os.path.dirname(ruta_superior)
-print(ruta_sup_2)
+###############################################################################
+#                                Funciones                                    #
+###############################################################################
+
 def path_verify(path):
   if not os.path.exists(path):
     os.makedirs(path)
     print(f'Directorio Creado: {path}')
   else:
     print(f'El Directorio ya existe: {path}')
-##
-###
-ruta_dumps = os.path.join( ruta_sup_2,f'public/recargas/semanas/{y}/{sem}')
-path_verify(ruta_dumps)
+## end path_verify
+def gen_report_by_status(df):
+  list_user_set = set(df['operation_external_reference'])
+  resultados = []
+  for mail in list_user_set:
+    df_user = df[df['operation_external_reference'] == mail]
+    rein = len(df_user)
+    mto_ctc = sum(df_user['operation_amount'])
+    resultados.append({
+      'Usuario': mail,
+      'Reincidencias': rein,
+      'Monto Total': mto_ctc
+    })
+  return resultados
+## end gen_report_by_status
+def no_ban(row,correos_baneados):
+  correo = row['operation_external_reference']
+  if correo in correos_baneados:
+    return True
+  else:
+    return False
+## end no_ban
+def es_sospechoso(row, correos_baneados, umbral_similitud=80):
+  correo = row['operation_external_reference']
+  usuario = correo.split('@')[0]
+  if correo in correos_baneados:
+    return True
+  for correo_baneado in correos_baneados:
+    usuario_baneado = correo_baneado.split('@')[0]
+    if fuzz.ratio(usuario, usuario_baneado) >= umbral_similitud:
+      return True
+  return False
+## end es_sospechoso
+def df_by_status(df,file,detail):
+  print(f'Creando JSON {detail}')
+  lista_operation_id = df['operation_id'].tolist()
+  monto = sum(df['operation_amount'])
+  print(detail,len(lista_operation_id),'Monto Total: $',monto)
+  with open(file, 'w') as f:
+      json.dump(lista_operation_id, f)
+## end df_by_status
+def search_date(df,fechas,fecha_limit,array):
+  for fecha in fechas:
+    if fecha >= fecha_limit:
+      df_dia = df[df['operation_date_created'] == fecha]
+      array.append(df_dia)
+## end search_date
+###############################################################################
+#                                   Paths                                     #
+###############################################################################
+
+## Obtener la ruta del directorio actual
+ruta_actual = os.getcwd()
+## Subir un nivel en el directorio
+ruta_superior = os.path.dirname(ruta_actual)
+## archivos json correos baneados
+ruta_bans = os.path.join(ruta_actual,'scripts/scriptsContracargos/data')
+## archivo csv contracargos semanal
+path_files = os.path.join(ruta_superior,f'dataFiles/contracargos/{y}')
+## salida de archivos
+ruta_dumps = os.path.join( ruta_actual,f'public/recargas/semanas/{y}/{sem}')
+## Rutas de archivos de salida
 dump_file = os.path.join(ruta_dumps,file_dump)
 dump_file_won = os.path.join(ruta_dumps,file_won)
 dump_file_lost = os.path.join(ruta_dumps,file_lost)
 dump_file_dispute = os.path.join(ruta_dumps,file_dispute)
-ruta_sup_root = os.path.dirname(ruta_sup_2)
-path_files = os.path.join(ruta_sup_root,f'dataFiles/contracargos/{y}')
-json_bans = os.path.join(ruta_bans,file_bans)
-####
+### Ruta json
+## json emails bans
+json_bans_v1 = os.path.join(ruta_bans,file_bans_v1)
+with open(json_bans_v1, 'r') as f:
+    data_bans_1 = json.load(f)
+## json emails bans
+json_bans_v2 = os.path.join(ruta_bans,file_bans_v2)
+with open(json_bans_v2, 'r') as f:
+    data_bans_2 = json.load(f)
+## archivo contracargos
 path_file = os.path.join(path_files,f'{file}.csv')
 df = pd.read_csv(path_file)
 ######
+path_verify(ruta_dumps)
 
-def gen_report_by_status(df):
-    list_user_set = set(df['operation_external_reference'])
-    resultados = []
-    for mail in list_user_set:
-        df_user = df[df['operation_external_reference'] == mail]
-        rein = len(df_user)
-        mto_ctc = sum(df_user['operation_amount'])
-        resultados.append({
-            'Usuario': mail,
-            'Reincidencias': rein,
-            'Monto Total': mto_ctc
-        })
-    return resultados
-######
-
+###############################################################################
+#                       Formateo de informacion                               #
+###############################################################################
 df.rename(
   columns={
     'Fecha de Creación del contracargo (date_created)': 'date_created', 
@@ -82,7 +134,6 @@ df.rename(
     'Plataforma (operation_marketplace)': 'operation_marketplace',
     }, inplace=True)
 ## Recortar dataframe
-
 df = df[[
   'date_created',
   'chargeback_id',
@@ -100,62 +151,62 @@ df = df[[
 # Ensure date_created is datetime format
 df['date_created'] = pd.to_datetime(df['date_created'], format='%d/%m/%Y %H:%M:%S')
 df['operation_date_created'] = pd.to_datetime(df['operation_date_created'], format='%d/%m/%Y %H:%M:%S')
-
-##
-with open(json_bans, 'r') as f:
-    data_bans = json.load(f)
-print('Buscando correos no baneados y reincidentes...')
-def no_ban(row,correos_baneados):
-  correo = row['operation_external_reference']
-  if correo in correos_baneados:
-    return True
-  else:
-    return False
-   
-df['No_ban'] = df.apply(no_ban, axis=1 , args=(data_bans,))
+###############################################################################
+#                       Formateo de informacion                               #
+###############################################################################
+print('Analizando correos baneados...')
+## busca correos unicos
+lista_final = list(set(data_bans_1) | set(data_bans_2))
+## aplica funcion de no_ban al DF
+df['No_ban'] = df.apply(no_ban, axis=1 , args=(lista_final,))
+## buscar correos no baneados y crear un DataFrame de estos
 nobans = df[df['No_ban'] == False]
+## extraer correos unicos de los no baneados
 corr_nb = set(nobans['operation_external_reference'])
+## verificar que no existan correos no baneados en la lista final de baneos
+## agregar nuevos baneos
 new_bans = []
+print('Buscando nuevos correos a banear...')
 for user in corr_nb:
   df_user = nobans[nobans['operation_external_reference'] == user]
-  # new_bans.append(df_user)
   reincidencias = len(df_user)
   monto = sum(df_user['operation_amount'])
-  if reincidencias >= 2 and monto >= 200:
+  if reincidencias >= 1 and monto >= 15:
     new_bans.append({
       'Usuario': user,
       'Reincidencias': reincidencias,
       'Monto': monto
     })
-    
+## crear DF de correos a banear
 df_to_ban = pd.DataFrame(new_bans)
+
+df_to_ban_list = df_to_ban['Usuario'].unique().tolist()
+print("Correos a Banear:",len(df_to_ban_list))
+lista_x = [[e for e in df_to_ban_list if e in lista_final]]
+if len(lista_x) > 1:
+  print("Correos a banear en baneados",len(lista_x))
 print('Buscando operaciones recientes..')
-###
+##
 fechas = set(df['operation_date_created'])
-news = []
-for fecha in fechas:
-  if fecha >= fecha_limit:
-    df_dia = df[df['operation_date_created'] == fecha]
-    news.append(df_dia)
-df_news = pd.concat(news)
-
-# Cargar datos (como en el ejemplo anterior)
+##
+re1 = []
+search_date(df,fechas,fecha_limit_1,re1)
+re2 = []
+search_date(df,fechas,fecha_limit_2,re2)
 print('Buscando correos sospechosos en nuevas operaciones...')
-def es_sospechoso(row, correos_baneados, umbral_similitud=80):
-    correo = row['operation_external_reference']
-    usuario = correo.split('@')[0]
-    if correo in correos_baneados:
-        return True
-    for correo_baneado in correos_baneados:
-        usuario_baneado = correo_baneado.split('@')[0]
-        if fuzz.ratio(usuario, usuario_baneado) >= umbral_similitud:
-            return True
-    return False
-
-df_news['sospechoso'] = df_news.apply(es_sospechoso, axis=1, args=(data_bans,))
-
+##
+df_news_1 = pd.concat(re1)
+## 
+df_news_1['sospechoso'] = df_news_1.apply(es_sospechoso, axis=1, args=(data_bans_1,))
 # Filtrar los registros sospechosos
-sospechosos = df_news[df_news['sospechoso']]
+# print(df_news_1['sospechoso'].value_counts())
+sospechosos1 = df_news_1[df_news_1['sospechoso'] == True]
+# print(sospechosos1)
+if len(re2) > 0:
+  df_news_2 = pd.concat(re2)
+  df_news_2['sospechoso'] = df_news_2.apply(es_sospechoso, axis=1, args=(data_bans_2,))
+  sospechosos2 = df_news_2[df_news_2['sospechoso'] == True]
+
 # print(sospechosos)
 print('Creando resumen General...')
 ##
@@ -165,14 +216,14 @@ lista_montos = df['operation_amount'].unique().tolist()
 lista_montos.sort(reverse=True)
 montos = []
 for monto in lista_montos:
-    df_mto = df[df['operation_amount'] == monto]
-    num = len(df_mto)
-    tt = monto * num
-    montos.append({
-        'Monto': monto,
-        'N° Transacciones': num, 
-        'Total': tt
-    })
+  df_mto = df[df['operation_amount'] == monto]
+  num = len(df_mto)
+  tt = monto * num
+  montos.append({
+      'Monto': monto,
+      'N° Transacciones': num, 
+      'Total': tt
+  })
 df_montos = pd.DataFrame(montos)    
 
 # print(df_montos)
@@ -196,28 +247,24 @@ df_rei_ord = df_rei_fn.sort_values(by='Reincidencias',ascending=False)
 df_cov_ord = df_cov_fn.sort_values(by='Reincidencias',ascending=False)
 
 with pd.ExcelWriter(dump_file) as writer:
-    df_montos.to_excel(writer, index=False ,sheet_name=f'Resumen Montos')
-    df_gen_ord.to_excel(writer, index=False ,sheet_name=f'Resumen General')
-    df_set_ord.to_excel(writer, index=False ,sheet_name=f'Resumen settled')
-    df_dis_ord.to_excel(writer, index=False ,sheet_name=f'Resumen dispute')
-    df_rei_ord.to_excel(writer, index=False ,sheet_name=f'Resumen reimbursed')
-    df_cov_ord.to_excel(writer, index=False ,sheet_name=f'Resumen covered')
-    df_to_ban.to_excel(writer,index=False,sheet_name='Reincidentes')
-    if len(sospechosos) > 0:
-      sospechosos.to_excel(writer,index=False,sheet_name='Sospechosos')
+  df_montos.to_excel(writer, index=False ,sheet_name=f'Resumen Montos')
+  df_gen_ord.to_excel(writer, index=False ,sheet_name=f'Resumen General')
+  df_set_ord.to_excel(writer, index=False ,sheet_name=f'Resumen settled')
+  df_dis_ord.to_excel(writer, index=False ,sheet_name=f'Resumen dispute')
+  df_rei_ord.to_excel(writer, index=False ,sheet_name=f'Resumen reimbursed')
+  df_cov_ord.to_excel(writer, index=False ,sheet_name=f'Resumen covered')
+  df_to_ban.to_excel(writer,index=False,sheet_name='Reincidentes')
+  if len(sospechosos1) > 0:
+    sospechosos1.to_excel(writer,index=False,sheet_name='Baneos 60')
+  # if len(sospechosos2) > 0:
+    #   sospechosos2.to_excel(writer,index=False,sheet_name='Sospechosos 397')
 
 ##
 df_won = df[df['status'] == 'reimbursed']
 df_lost = df[(df['status'] == 'settled') | (df['status'] == 'covered') ]
 df_dispute = df[(df['status'] == 'dispute') | (df['status'] == 'documentation_pending') ]
 
-def df_by_status(df,file,detail):
-  print(f'Creando JSON {detail}')
-  lista_operation_id = df['operation_id'].tolist()
-  monto = sum(df['operation_amount'])
-  print(detail,len(lista_operation_id),'Monto Total: $',monto)
-  with open(file, 'w') as f:
-      json.dump(lista_operation_id, f)
+
 
 df_by_status(df_won,dump_file_won,won)
 df_by_status(df_lost,dump_file_lost,lost)

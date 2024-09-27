49c9b547832788716b9b08f4b4f1f0198f83b362
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-09-26 19:10:40 -0600

    NewScriptValidacion_v1

--- scripts/scriptsTest/scriptsTestValidadores/validacion_full.py
+++ scripts/scriptsTest/scriptsTestValidadores/validacion_full.py
@@ -5,102 +5,145 @@ import json
 #                           Carga de archivos                                 #
 ###############################################################################
 ## Definimos el mes con nombre
-mes = "Julio"
+mes = "Septiembre"
 ## Definimos el mes con número
-m = "07"
+m = "09"
 ## Definimos el año
 y = "2024"
+folder_p = 'semanas'
+# folder_p = 'quincenas'
+periodo = 'semana 38'
+# periodo = '1ra qna'
+# periodo = '2da qna'
 ## Nombres de archivos
-name_file = "ort_2da qna Julio"
-
-ruta_actual = os.path.dirname(__file__)
-## Subir un nivel en el directorio
-ruta_superior = os.path.dirname(ruta_actual)
-ruta_sup_2 = os.path.dirname(ruta_superior)
-ruta_sup_3 = os.path.dirname(ruta_sup_2)
-ruta_sup_4 = os.path.dirname(ruta_sup_3)
-
-## Ruta donde se leen y se guardan los archivos csv
-## Ruta donde se leen los archivos json
-path_dics = "data"
-json_int = "integradores.json"
-path_json_int = os.path.join(path_dics,json_int)
-json_tot = "transacciones.json"
-path_json_tot = os.path.join(path_dics,json_tot)
-## Carga de archivo csv para analisis 
-path_files = os.path.join(ruta_sup_4,f'dataFiles/validaciones/{y}/{m} {mes}')
+name_file = "Validaciones semana 38 2024"
 file = f'{name_file}.csv'
+json_int = "integradores.json"
+json_tot = "transacciones.json"
+#
+col_n_l = 'Linea'
+col_n_f = 'Fecha'
+##
+linea = 'LINEA'
+dato_fec = 'FECHA_HORA_TRANSACCION'    
+tot_v = 'TIPO_TRANSACCION' 
+####################################################################################################
+def path_verify(path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+    print(f'Directorio Creado: {path}')
+  else:
+    print(f'El Directorio ya existe: {path}')
+## end path_verify
+## Modificar Lineas con base al JSON de Integradores
+def remplazo_valores(data_json,df,column):
+  for origen,replacements in data_json.items():
+    for origen_dict in replacements:
+      for codigo, replacement in origen_dict.items():
+        df.replace({f'{column}':codigo},replacement,inplace=True)
+## end remplazo_valores    
+def resumen(lista,df,n_col,lista_tp):
+  dic_el = []
+  for element in lista:
+    # print(element)
+    # print(n_col)
+    df_element = df[df[f'{n_col}'] == element]
+    # print(df_element['TIPO_TRANSACCION'].value_counts())
+    df_bus = df_element[df_element['TIPO_TRANSACCION'] == 'Debito en Bus']
+    df_ban = df_element[df_element['TIPO_TRANSACCION'] == 'Debito en bano']
+    mto_bus = df_bus['MONTO_TRANSACCION'].sum() / 100
+    mto_ban = df_ban['MONTO_TRANSACCION'].sum() / 100
+    # print(mto_bus)
+    # print(mto_ban)
+    dic = []
+    dic.append({'Monto Autobus': mto_bus} )
+    dic.append({'Monto Bano': mto_ban} )
+    for tot in lista_tp:
+      df_tot =df_element[df_element['TIPO_TRANSACCION'] == tot]
+      z = len(df_tot)
+      dic.append({tot: z})
+    dic_el.append({element:dic})
+  # print(dic_el)
+  return dic_el
+## end resumen
+def read_list(lista,col_name):
+  # print(col_name)
+  data = []
+  for key_l in lista:
+    for key_d in key_l:
+      data_key_e = key_l[f'{key_d}'] 
+      # print(key_d)
+      for element in data_key_e:
+        label = list(element.keys())[0]
+        cantidad = element[label]
+        # print(label)
+        # print(cantidad)
+        data.append({
+        f'{col_name}': key_d,
+        f'{label}': cantidad    
+        })
+  df_list = pd.DataFrame(data)
+  df_grouped = df_list.groupby(col_name).sum()
+  df_grouped = df_grouped.reset_index()
+# Aplicar la solución
+  columnas_numericas = df_grouped.select_dtypes(include=['number']).columns
+  # print(columnas_numericas)
+  total_row = pd.DataFrame(df_grouped[columnas_numericas].sum()).T
+  total_row[f'{col_name}'] = 'Total'
+  df_final = pd.concat([df_grouped, total_row], ignore_index=True)
+  return df_final
+## end read_list
+
+####################################################################################################
+ruta_actual = os.getcwd()
+print(ruta_actual)
+parent_dir = os.path.dirname(ruta_actual)
+# ## Subir un nivel en el directorio
+# ## Produccion
+ruta_test_json = os.path.join(ruta_actual,'scripts/scriptsTest/scriptsTestValidadores/data')
+path_files = os.path.join(ruta_actual,f'scripts/scriptsTest/scriptsTestValidadores/data/{y}/{m} {mes}')
+# path_files = os.path.join(parent_dir,f'dataFiles/validaciones/{y}/{m} {mes}')
+path_dumps = os.path.join(ruta_actual,f'public/validaciones/{folder_p}/{y}')
+path_verify(path_dumps)
+
+## Ruta donde se leen los archivos json
+path_json_int = os.path.join(ruta_test_json,json_int)
+path_json_tot = os.path.join(ruta_test_json,json_tot)
 path_file = os.path.join(path_files, file)
-df = pd.read_csv(path_file, low_memory=False)
+## Carga de archivo csv para analisis 
+df = pd.read_csv(path_file,encoding='latin-1', low_memory=False)
 ## Load JSON data integradores
 with open(path_json_int) as f:
     data_int = json.load(f)
 ## Load JSON data integradores
 with open(path_json_tot) as f:
     data_tot = json.load(f)
+df['TIPO_TRANSACCION'] = df['TIPO_TRANSACCION'].astype('str')
 ###############################################################################
 #                           Modificacion de Formatos                          #
 ###############################################################################
-## Modificar Lineas con base al JSON de Integradores
-for company, replacements in data_int.items():
-  for company_dict in replacements:
-    for codigo,replacement in company_dict.items():
-      df.replace({'LINEA':codigo}, replacement, inplace=True)
-## Crear Lista de las LINEAS
-lineas = set(df['LINEA'])  
-# print(lineas)   
+## Funciones de modificacion de datos
+remplazo_valores(data_tot,df,tot_v)
+remplazo_valores(data_int,df,linea)
+## Crear Listas a utilizar
+lineas = df['LINEA'].unique().tolist()
+tipos = df['TIPO_TRANSACCION'].unique().tolist()
 ## Modificar formato de las fechas
-df['TIPO_TRANSACCION'] = df['TIPO_TRANSACCION'].astype('str')
-df['FECHA_HORA_TRANSACCION'] = pd.to_datetime(df['FECHA_HORA_TRANSACCION'],format="mixed")  
+df['FECHA_HORA_TRANSACCION'] = pd.to_datetime(df['FECHA_HORA_TRANSACCION'],format='mixed')  
 df['FECHA_HORA_TRANSACCION'] = df['FECHA_HORA_TRANSACCION'].dt.strftime('%Y-%m-%d')
-# ## Obtener fechas unicas
+## Obtener fechas unicas
 fechas_unicas = sorted(set(df['FECHA_HORA_TRANSACCION']))
-# print(fechas_unicas)
-###############################################################################
-#                 Definir Funciones utiles para el resumen                    #
-###############################################################################
-## funcion para realizar 
-def process_transactions(tot, df_dato, dia):
-  for transaccion in data_tot[tot]:
-    print(transaccion)
-    for codigo,details in transaccion.items():
-      print(codigo)
-      print(details)
-      df_tot = df_dato[df_dato['TIPO_TRANSACCION'] == codigo]
-      size = len(df_tot)
-      dia.append({
-        f'{details}': size
-      })
-    
-def resumen(lista,df,datos):
-  tr = [] 
-  for dato in lista:
-    df_dato = df[df[f'{datos}'] == dato]
-    dia = []
-    process_transactions('exitosas',df_dato,dia)
-    process_transactions('erroneas',df_dato,dia)
-    df_day = pd.DataFrame(dia)
-    print(df_day)
-    # tr.append({
-    #   f'{datos}': dato,
-    #     {dia[0]}: dia
-    # })
-    print(dato,dia)
-  # return tr
-        # print(dato,details,len(df_tot))
-    # print(dato)
-    # print(df_dato)
-    
-  #   process_transactions("exitosas",df_dato,tr,datos,dato)
-  #   # process_transactions("erroneas",df_dato,tr,datos,dato)
-  # df_res = pd.DataFrame(tr)
-  # return df_res    
-###############################################################################
-#                            Uso de funciones                                 #
-###############################################################################   
-dato_lin = 'LINEA'    
-dato_fec = 'FECHA_HORA_TRANSACCION'    
-# print(resumen(lineas,df,dato_lin))
-# print('')
-res_c = pd.DataFrame(resumen(fechas_unicas,df,dato_fec))
-print(res_c)
+##  Aplicar Resumen
+fechas = resumen(fechas_unicas,df,dato_fec,tipos)
+lineas = resumen(lineas,df,linea,tipos)
+## Leer listas de resumen
+df_fechas = read_list(fechas,col_n_f)
+df_lineas = read_list(lineas,col_n_l)
+## Datos archivo Final
+lista = f"res_validaciones_{periodo}_{m}_{y}.xlsx"
+ruta_lista = os.path.join(path_dumps,lista)
+## Generacion de archivo de resultados
+with pd.ExcelWriter(ruta_lista) as writer:
+  df_fechas.to_excel(writer, index=False ,sheet_name=f'Resumen Validaciones ')
+  df_lineas.to_excel(writer, index=False ,sheet_name=f'Resumen Linea ')
+print('Proceso realizado con exito')

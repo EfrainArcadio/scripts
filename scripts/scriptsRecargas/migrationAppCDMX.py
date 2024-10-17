bef5a080373bc4495d5b49a7974e30e210c1dc6f
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-10-17 17:39:05 -0600

    Migration_SEPT_REV

--- scripts/scriptsRecargas/migrationAppCDMX.py
+++ scripts/scriptsRecargas/migrationAppCDMX.py
@@ -34,11 +34,11 @@ with open(json_conn, 'r') as f_conn:
 connection = psycopg2.connect(**data_conn)
 ##
 tu1 = connection.cursor()
-tu1.execute("SELECT DISTINCT numero_serie_hex FROM datos_rre_2023 WHERE fecha_hora_transaccion >= '2023-11-01 00:00:00' AND fecha_hora_transaccion <= '2023-12-31 23:59:59'  AND tipo_transaccion = '0' AND location_id IN ('101800','201A00') AND tipo_transaccion IS NOT NULL UNION SELECT DISTINCT numero_serie_hex FROM datos_rre_2024 WHERE fecha_hora_transaccion >= '2024-01-01 00:00:00' AND fecha_hora_transaccion <= '2024-08-31 23:59:59' AND tipo_transaccion = '0' AND location_id IN ('101800','201A00') AND tipo_transaccion IS NOT NULL ")
+tu1.execute("SELECT DISTINCT numero_serie_hex FROM datos_rre_2023 WHERE fecha_hora_transaccion >= '2023-11-01 00:00:00' AND fecha_hora_transaccion <= '2023-12-31 23:59:59'  AND tipo_transaccion = '0' AND location_id IN ('101800','201A00') AND tipo_transaccion IS NOT NULL UNION SELECT DISTINCT numero_serie_hex FROM datos_rre_2024 WHERE fecha_hora_transaccion >= '2024-01-01 00:00:00' AND fecha_hora_transaccion <= '2024-09-30 23:59:59' AND tipo_transaccion = '0' AND location_id IN ('101800','201A00') AND tipo_transaccion IS NOT NULL ")
 tup1 = tu1.fetchall()
 
 tu2 = connection.cursor()
-tu2.execute("SELECT DISTINCT numero_serie_hex FROM datos_rre_2024 WHERE fecha_hora_transaccion >= '2024-07-01 00:00:00' AND fecha_hora_transaccion <= '2024-08-31 23:59:59' AND tipo_transaccion = '0' AND location_id IN ('101801') AND tipo_transaccion IS NOT NULL")
+tu2.execute("SELECT DISTINCT numero_serie_hex FROM datos_rre_2024 WHERE fecha_hora_transaccion >= '2024-07-01 00:00:00' AND fecha_hora_transaccion <= '2024-09-30 23:59:59' AND tipo_transaccion = '0' AND location_id IN ('101801') AND tipo_transaccion IS NOT NULL")
 tup2 = tu2.fetchall()
 ##
 connection.close()
@@ -46,18 +46,22 @@ connection.close()
 cards_pe1 = []
 for card in tup1:
   cards_pe1.append(card[0])
+# print(cards_pe1)
 print(f'Total de Tarjetas unicas RRD y RRF P1: {len(cards_pe1)}')
 ##
 cards_pe2 = []
 for card in tup2:
   cards_pe2.append(card[0])
+# print(cards_pe2)
+  
 print(f'Total de Tarjetas unicas AppCDMX P2: {len(cards_pe2)}')
 
-lista_app_in_rre = [ e for e in cards_pe2 if e in cards_pe1 ]
-
+lista_app_in_rre = (  e for e in cards_pe2 if e in cards_pe1 )
+lista_app_in_rre = list(lista_app_in_rre)
 print(f'Total de tarjetas de App en RRD y RRF {len(lista_app_in_rre)}')
 
-lista_app_not_in_rre = [ e for e in cards_pe2 if not e in cards_pe1 ]
+lista_app_not_in_rre = (  e for e in cards_pe2 if not e in cards_pe1 )
+lista_app_not_in_rre = list(lista_app_not_in_rre)
 print(f'Total de tarjetas Solo de App {len(lista_app_not_in_rre)}')
   
 

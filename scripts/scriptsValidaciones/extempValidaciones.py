fd76c73e71c04e25735b3f99112e2fa31e658dbe
EfrainArcadio <efrain.arcadio@ort.cdmx.gob.mx>
2024-10-28 17:10:29 -0600

    Extemp_val_realize

--- scripts/scriptsValidaciones/extempValidaciones.py
+++ scripts/scriptsValidaciones/extempValidaciones.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import os
+import numpy as np
 #### Variables
 ## fechas
-y = '2024'
-m = '02'
-mes = 'Febrero'
+y = '2023'
+m = '12'
+mes = 'Diciembre'
 ## periodo
 # qna = '1ra'
 qna = '2da'
 ## archivos
-name_file_v1 = 'Validaciones 2da qna febrero 2024'
-name_file_v2 = '2da_qna_Febrero_2024_v2'
-######### Funciones #####
+name_file_v1 = 'ORT_Validaciones_2da_qna_diciembre_2023_gral'
+name_file_v2 = '2da_qna_Diciembre_2023_v2'
+######### Funciones #####     
 def path_verify(path):
   if not os.path.exists(path):
     os.makedirs(path)
@@ -44,11 +45,22 @@ file_v2 = os.path.join(path_files,f'{name_file_v2}.csv')
 df_file_v1 = pd.read_csv(file_v1,low_memory=False, encoding='latin-1')
 df_file_v2 = pd.read_csv(file_v2,low_memory=False, encoding='latin-1')
 ########### Procesamiento de datos
+# df_file_v1['ID_TRANSACCION_ORGANISMO'] = df_file_v1['ID_TRANSACCION_ORGANISMO'].str.replace('.', '')
+# df_file_v2['ID_TRANSACCION_ORGANISMO'] = df_file_v2['ID_TRANSACCION_ORGANISMO'].str.replace('.', '')
+df_file_v1['ID_TRANSACCION_ORGANISMO'] = df_file_v1['ID_TRANSACCION_ORGANISMO'].astype('Int64')
+df_file_v2['ID_TRANSACCION_ORGANISMO'] = df_file_v2['ID_TRANSACCION_ORGANISMO'].astype('Int64')
+df_file_v2 = df_file_v2.dropna(subset=['ID_TRANSACCION_ORGANISMO'])
+df_file_v1 = df_file_v1.dropna(subset=['ID_TRANSACCION_ORGANISMO'])
+print('Vacios',df_file_v1['ID_TRANSACCION_ORGANISMO'].isnull().sum())
+print('Vacios',df_file_v2['ID_TRANSACCION_ORGANISMO'].isnull().sum())
+# print(df_file_v1.info())
+# print(df_file_v2.info())
 ## Crear listas de los ID_TRANSACCION_ORGANISMO 
 id_tr1 = df_file_v1['ID_TRANSACCION_ORGANISMO'].unique().tolist()
 id_tr2 = df_file_v2['ID_TRANSACCION_ORGANISMO'].unique().tolist()
 ## Buscar las nuevas transacciones
-
+id_tr1 = np.array(id_tr1)
+id_tr2 = np.array(id_tr2)
 l1 = len(id_tr1)
 print('Transacciones Primer Archivo',len(id_tr1))
 ## Buscar longitud de la segunda lista
@@ -58,8 +70,11 @@ print('Transacciones Segundo Archivo',len(id_tr2))
 print('Diferencia = # datos esperados',l2 - l1)
 ##
 print('Buscado Nuevas Transacciones ...')
-list_ext = ( e for e in id_tr2 if e not in id_tr1 )
-list_no_2 = ( e for e in id_tr1 if e not in id_tr2 )
+list_ext = id_tr2[~np.isin(id_tr2, id_tr1)].tolist()
+# list_ext = ( e for e in id_tr2 if e not in id_tr1 )
+
+# list_no_2 = ( e for e in id_tr1 if e not in id_tr2 )
+list_no_2 = id_tr1[~np.isin(id_tr1, id_tr2)].tolist()
 ## Nuevos Resultados  
 ##
 df_extemp = tr_verify(df_file_v2,list_ext)
@@ -70,13 +85,13 @@ print('Generando archivo extemporaneos')
 print(f'Generando CSV: {file_ext}')
 ruta_ext = os.path.join(path_dumps,file_ext)
 df_extemp.to_csv(ruta_ext,index=False)
-print(df_extemp['LINEA'].value_counts())
+# print(df_extemp['LINEA'].value_counts())
 lin_f = df_extemp['LINEA'].unique().tolist()
 df_extemp['TIPO_TRANSACCION'] = df_extemp['TIPO_TRANSACCION'].astype(str)
+
 res_f = []
 for linea in lin_f:
   df_lf = df_extemp[df_extemp['LINEA'] == linea]
-  # print(df_lf['TIPO_TRANSACCION'].value_counts())
   df_valids = df_lf[(df_lf['TIPO_TRANSACCION'] == '5') | (df_lf['TIPO_TRANSACCION'] == '3')]
   monto_ext = sum(df_valids['MONTO_TRANSACCION']) / 100
   res_f.append({
@@ -90,6 +105,7 @@ file_rext = f're_extemp_{qna}_qna_{mes}.csv'
 ruta_rext = os.path.join(path_dumps,file_rext)
 df_res_f.to_csv(ruta_rext,index=False)
 
+print(f'Generando Resumen {file_rext}')
 
 ## condicional para faltantes
 if any(list_no_2):
